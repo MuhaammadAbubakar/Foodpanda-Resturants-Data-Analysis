46178db5f653c7c967085e8b6d66a300cdbde4ea
MuhammadAbubakar <patanai2412@gmail.com>
2026-05-20 11:08:27 +0500

    mainfile

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\rajas\Desktop\Foodpanda Project\Foodpanda-Resturants-Data-Analysis\Foodpanda Analysis Dataset.csv"
+)  
+
+print(df.head())
+
+print(df.describe())
