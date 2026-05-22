f4fcf208d2d8101f9c53fdaed768f48f9e3ae1db
MuhammadAbubakar <patanai2412@gmail.com>
2026-05-22 12:06:42 +0500

    Update

--- main.py
+++ main.py
@@ -6,3 +6,30 @@ df = pd.read_csv(r"C:\Users\rajas\Desktop\Foodpanda Project\Foodpanda-Resturants
 print(df.head())
 
 print(df.describe())
+print(df.info())
+print(df.tail())
+print(df.isnull().sum())
+print(df.duplicated().sum())
+print(df.columns)
+
+print(df["rating"].dtype)
+
+#Average rating of all orders
+def average_rating():
+  print(df["rating"].mean())
+
+average_rating()
+
+#Top rating in all orders
+def top_rating():
+  top = df.sort_values(by ="rating", ascending=False)
+  print(top.head(50))
+
+top_rating()
+
+#Most Common City
+def most_common_city():
+    common_city = df["city"].value_counts()
+    print(common_city)
+
+most_common_city()
